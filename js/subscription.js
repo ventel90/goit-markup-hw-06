6579277cd4c036a28fdef6d03fd93b0a9152f049
ventel90 <ventel90@gmail.com>
2022-07-24 15:48:39 +0300

    24.07.2022

--- js/subscription.js
+++ js/subscription.js
@@ -1,5 +1,5 @@
 (() => {
-	document.querySelector(".js-speaker-form").addEventListener("submit", e => {
+	document.querySelector(".footer-form").addEventListener("submit", e => {
 		e.preventDefault();
 
 		new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
